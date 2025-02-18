055a91f50f5431ad9e96e831d5265e949fb5f2fd
Sharabhoj <sharabhoj@hotmail.com>
2025-02-17 21:42:59 -0500

    fixed connection loop. added TUN interface functions

--- client.c
+++ client.c
@@ -2,84 +2,136 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main (int argc, char* argv[ ])		// Three arguments to be checked later
+#define BUFLEN  512                       // Maximum length of buffer
+#define TUN_NAME "vpn0"                   // Name for our TUN interface
+#define MTU     1500                      // Standard MTU size
+
+// Function to create and configure TUN interface
+int tun_create(char *dev_name) {
+    struct ifreq ifr;
+    int fd;
+
+    // Open the clone device
+    if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
+        perror("Opening /dev/net/tun failed");
+        return fd;
+    }
+
+    // Set up interface request structure
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;  // TUN device, no packet info
+    if (dev_name)
+        strncpy(ifr.ifr_name, dev_name, IFNAMSIZ);
+
+    // Create the interface
+    if (ioctl(fd, TUNSETIFF, (void *)&ifr) < 0) {
+        perror("ioctl TUNSETIFF failed");
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+}
+
+int main (int argc, char* argv[ ])       // Two arguments needed: IP and port
 {
-	// Declarations and definitions
-	int sock;				// Socket descriptor
-	int n = 0;				// Number of bytes in each recv call
-	struct sockaddr_in servAddr;		// Server socket address data structure
-	char* servIP = argv[1];			// Server IP address from command line
-	int servPort = atoi(argv[2]);		// Server port number from command line
-	char* message = argv[3];		// Message specified on the command line
-	int len = 0;				// Length of string to be echoed
-	char buffer[512 + 1];			// Buffer
-	char* ptr = buffer;			// Pointer to buffer
-	int max_len = sizeof(buffer);		// Maximum number of bytes to send
+    // Declarations and definitions
+    int sock;                            // Socket descriptor
+    int tun_fd;                          // TUN interface file descriptor
+    int n = 0;                           // Number of bytes in each recv call
+    struct sockaddr_in servAddr;         // Server socket address data structure
+    char* servIP = argv[1];              // Server IP address from command line
+    int servPort = atoi(argv[2]);        // Server port number from command line
+    char buffer[BUFLEN + 1];             // Buffer for TCP data
+    char tun_buffer[MTU];                // Buffer for TUN packets
+    char* ptr = buffer;                  // Pointer to buffer
+    int max_len = sizeof(buffer);        // Maximum number of bytes to send
 
-	// Check for correct number of command line arguments
-	if(argc != 4) {                                               
-		printf("client [IP address] [Port] [Message]\n");                             
-		exit (1);
-	}                                                
+    // Check for correct number of command line arguments
+    if(argc != 3) {                                               
+        printf("client [IP address] [Port]\n");                             
+        exit (1);
+    }                                                
 
-	// Populate socket address for the server
-	printf("Populating server socket address..\n");
-	memset (&servAddr, 0, sizeof(servAddr));	// Initialize data structure
-	servAddr.sin_family = AF_INET; 			// This is an IPv4 address
-	servAddr.sin_addr.s_addr = inet_addr(servIP); 	// Server IP address
-	servAddr.sin_port = htons(servPort);		// Server port number
-	
-	// Create a TCP socket stream
-	printf("Creating TCP socket stream..\n");
-	if ((sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
-		printf("Error: socket creation failed!\n");
-		exit (1);
-	}
-	else
- 		printf("Socket successfully created..\n"); 
+    // Create TUN interface
+    printf("Creating TUN interface %s...\n", TUN_NAME);
+    if ((tun_fd = tun_create(TUN_NAME)) < 0) {
+        printf("Error creating TUN interface\n");
+        exit(1);
+    }
+    printf("TUN interface created successfully\n");
 
-	// Connect to the server
-	printf("Connecting to the server..\n");
-	if ((connect (sock, (struct sockaddr*)&servAddr, sizeof(servAddr))) == -1) {
-		printf("Error: connection to the server failed!\n");
-		exit (1);
-	}
-	else
-        	printf("Connected to the server..\n"); 
-	
-	// Send data to the server...
-	printf("Sending message to server: %s\n", message);
-	send(sock, message, strlen(message), 0);
+    // Populate socket address for the server
+    printf("Populating server socket address..\n");
+    memset (&servAddr, 0, sizeof(servAddr));         // Initialize data structure
+    servAddr.sin_family = AF_INET;                   // This is an IPv4 address
+    servAddr.sin_addr.s_addr = inet_addr(servIP);    // Server IP address
+    servAddr.sin_port = htons(servPort);             // Server port number
+    
+    // Create a TCP socket stream
+    printf("Creating TCP socket stream..\n");
+    if ((sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+        printf("Error: socket creation failed!\n");
+        exit (1);
+    }
+    else
+        printf("Socket successfully created..\n"); 
 
-	// Reset variables before receiving
-	ptr = buffer;
-	len = 0;
-	max_len = sizeof(buffer);
-	memset(buffer, 0, sizeof(buffer));
+    // Connect to the server
+    printf("Connecting to the server..\n");
+    if ((connect (sock, (struct sockaddr*)&servAddr, sizeof(servAddr))) == -1) {
+        printf("Error: connection to the server failed!\n");
+        exit (1);
+    }
+    else
+        printf("Connected to the server..\n"); 
+    
+    // Main communication loop
+    while(1) {
+        // Read from TUN interface
+        n = read(tun_fd, tun_buffer, MTU);
+        if (n > 0) {
+            // Get packet from TUN and send to server
+            printf("Received %d bytes from TUN\n", n);
+            send(sock, tun_buffer, n, 0);
+        }
 
-	// Receive data back from the server..
-	printf("Waiting for server to echo back message..\n");
-	while ((n = recv(sock, ptr, max_len, 0)) > 0) {		// Loop while receiving data... 
-		ptr += n;					// Move pointer along the buffer
-		max_len -= n;					// Adjust maximum number of bytes
-		len += n;					// Update length of string received
+        // Reset variables before receiving
+        ptr = buffer;
+        memset(buffer, 0, sizeof(buffer));
 
-		// print data...
-		buffer[len] = '\0'; 				// Null terminate
-		printf("Echoed string received: %s\n", buffer);	// Print buffer
-	}
+        // Receive response from the server..
+        printf("Waiting for server response..\n");
+        n = recv(sock, buffer, max_len, 0);
+        if(n <= 0) {
+            if(n < 0) {
+                printf("Error receiving data\n");
+            } else {
+                printf("Server disconnected\n");
+            }
+            break;
+        }
 
-	// Close socket
-	printf("Closing client connection..\n");
-	close (sock);
-	
-	// Stop program
-	printf("Stopping program..\n");
-	exit (0);
-	
+        // Write received data to TUN interface
+        printf("Writing %d bytes to TUN\n", n);
+        write(tun_fd, buffer, n);
+    }
+
+    // Close socket and TUN interface
+    printf("Closing client connection and TUN interface..\n");
+    close(sock);
+    close(tun_fd);
+    
+    // Stop program
+    printf("Stopping program..\n");
+    exit (0);
 }
--- server.c
+++ server.c
@@ -2,109 +2,165 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define	BUFLEN	512						// Maximum length of buffer
-#define PORT	9988						// Fixed server port number
+#define BUFLEN   512                       // Maximum length of buffer
+#define PORT     9988                      // Fixed server port number
+#define TUN_NAME "vpn0"                    // Name for TUN interface
+#define MTU      1500                      // Standard MTU size
 
-int main (void)
+// Function to create and configure TUN interface
+int tun_create(char *dev_name) {
+    struct ifreq ifr;
+    int fd;
+
+    // Open the clone device
+    if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
+        perror("Opening /dev/net/tun failed");
+        return fd;
+    }
+
+    // Set up interface request structure
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;  // TUN device, no packet info
+    if (dev_name)
+        strncpy(ifr.ifr_name, dev_name, IFNAMSIZ);
+
+    // Create the interface
+    if (ioctl(fd, TUNSETIFF, (void *)&ifr) < 0) {
+        perror("ioctl TUNSETIFF failed");
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+}
+
+int main(void)
 {
-	// Declarations and definitions
-	int listen_sock;					// Listen socket descriptor
-	int sock;						// Socket descriptor
-	char buffer[BUFLEN];					// Data buffer
-	char* ptr = buffer;					// Pointer to data buffer
-	int len = 0;						// Number of bytes to send/receive
-	int max_len = sizeof(buffer);				// Maximum number of bytes to receive
-	int n = 0;						// Number of bytes for each recv call
-	int wait_size = 16;					// Size of waiting clients
-	struct sockaddr_in server_address;			// Data structure for server address
-	struct sockaddr_in client_address;			// Data structure for client address
-	int client_address_len = sizeof(client_address);	// Length of client address
+    // Declarations and definitions
+    int listen_sock;                                    // Listen socket descriptor
+    int sock;                                           // Socket descriptor
+    int tun_fd;                                         // TUN interface file descriptor
+    char buffer[BUFLEN];                                // Data buffer
+    char tun_buffer[MTU];                               // Buffer for TUN packets
+    char* ptr = buffer;                                 // Pointer to data buffer
+    int len = 0;                                        // Number of bytes to send/receive
+    int max_len = sizeof(buffer);                       // Maximum number of bytes to receive
+    int n = 0;                                          // Number of bytes for each recv call
+    int wait_size = 16;                                 // Size of waiting clients
+    struct sockaddr_in server_address;                  // Data structure for server address
+    struct sockaddr_in client_address;                  // Data structure for client address
+    int client_address_len = sizeof(client_address);    // Length of client address
 
-	// Populate socket address for the server
-	printf("Populating server socket address..\n");
-	memset (&server_address, 0, sizeof(server_address));	// Initialize server address data structure
-	server_address.sin_family = AF_INET; 			// Populate family field - IPV4 protocol
-	server_address.sin_port = htons(PORT);			// Set port number
-	server_address.sin_addr.s_addr = INADDR_ANY;		// Set IP address to IPv4 value for loacalhost
-	
-	// Print server information (IP and port)
-	char* ip = inet_ntoa(server_address.sin_addr);
-	int port = ntohs(server_address.sin_port);
-	printf("IP Address: %s Port: %d\n", ip, port);
+    // Create TUN interface
+    printf("Creating TUN interface %s...\n", TUN_NAME);
+    if ((tun_fd = tun_create(TUN_NAME)) < 0) {
+        printf("Error creating TUN interface\n");
+        exit(1);
+    }
+    printf("TUN interface created successfully\n");
 
-	// Create a TCP socket; returns -1 on failure
-	printf("Creating TCP socket..\n");
-	if ((listen_sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
-		printf("Error: Listen socket failed!\n");
-		exit(1);
-	}
-	else
-		printf("Socket successfully created..\n");
+    // Populate socket address for the server
+    printf("Populating server socket address..\n");
+    memset(&server_address, 0, sizeof(server_address));   // Initialize server address data structure
+    server_address.sin_family = AF_INET;                  // Populate family field - IPV4 protocol
+    server_address.sin_port = htons(PORT);                // Set port number
+    server_address.sin_addr.s_addr = INADDR_ANY;          // Set IP address to IPv4 value for localhost
+    
+    // Print server information (IP and port)
+    char* ip = inet_ntoa(server_address.sin_addr);
+    int port = ntohs(server_address.sin_port);
+    printf("IP Address: %s Port: %d\n", ip, port);
 
-	// Bind the socket to the server address; returns -1 on failure
-	printf("Binding socket to server address..\n");
-	if ((bind(listen_sock, (struct sockaddr *)&server_address, sizeof(server_address))) == -1) {
-		printf("Error: binding failed!\n");
-		exit(1);
-	}
-	else
-		printf("Socket successfully bound..\n");
-	
-	// Listen for connections...
-	printf("Attempting to to listen for client connections..\n");
-	if (listen(listen_sock, wait_size) == -1) {
-		printf("Error: listening failed!\n");
-		exit(1);
-	}	
-	else
-		printf("Server is listening for connections..\n");
+    // Create a TCP socket; returns -1 on failure
+    printf("Creating TCP socket..\n");
+    if ((listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+        printf("Error: Listen socket failed!\n");
+        exit(1);
+    }
+    else
+        printf("Socket successfully created..\n");
 
-	// Handle the connection (run forever)
-	for(;;) {	
-		// Accept connections from client...
-		if ((sock = accept(listen_sock, (struct sockaddr *)&client_address, &client_address_len)) == -1) {
-			printf("Error: accepting failed!\n");
-			exit(1);
-		}
-		else
-			printf("Server has accepted a client connection..\n");
+    // Bind the socket to the server address; returns -1 on failure
+    printf("Binding socket to server address..\n");
+    if ((bind(listen_sock, (struct sockaddr*)&server_address, sizeof(server_address))) == -1) {
+        printf("Error: binding failed!\n");
+        exit(1);
+    }
+    else
+        printf("Socket successfully bound..\n");
+    
+    // Listen for connections...
+    printf("Attempting to listen for client connections..\n");
+    if (listen(listen_sock, wait_size) == -1) {
+        printf("Error: listening failed!\n");
+        exit(1);
+    }    
+    else
+        printf("Server is listening for connections..\n");
 
-		// Reset variables for new connection
-		ptr = buffer;
-		len = 0;
-		max_len = sizeof(buffer);
-		memset(buffer, 0, sizeof(buffer));
+    // Handle the connection (run forever)
+    for(;;) {    
+        // Accept connections from client...
+        if ((sock = accept(listen_sock, (struct sockaddr*)&client_address, &client_address_len)) == -1) {
+            printf("Error: accepting failed!\n");
+            exit(1);
+        }
+        else
+            printf("Server has accepted a client connection..\n");
 
-		// Receive and format the data...
-		printf("Receiving data from client..\n");
-		while ((n = recv(sock, ptr, max_len, 0)) > 0) {
-			// Print received data
-			buffer[n] = 0;		// Null terminate
-			printf("Server received: %s\n", buffer);
+        // Handle messages from this client until they disconnect
+        while(1) {
+            // Reset variables for new message
+            ptr = buffer;
+            len = 0;
+            max_len = sizeof(buffer);
+            memset(buffer, 0, sizeof(buffer));
 
-			ptr += n;			// Move pointer along buffer
-			max_len -= n;			// Adjust maximum number of bytes to receive
-			len += n;			// Update number of bytes received
+            // Read from TUN interface
+            n = read(tun_fd, tun_buffer, MTU);
+            if (n > 0) {
+                // Got packet from TUN, send to client
+                printf("Received %d bytes from TUN\n", n);
+                send(sock, tun_buffer, n, 0);
+            }
 
-			// Echo data back to the client...
-			send(sock, buffer, len, 0);	// Echo received bytes
-			close(sock);			// Close the socket
-		}
+            // Receive data from client...
+            printf("Receiving data from client..\n");
+            n = recv(sock, ptr, max_len, 0);
+            
+            // Check for client disconnect or error
+            if (n <= 0) {
+                if (n < 0) {
+                    printf("Error receiving data\n");
+                } else {
+                    printf("Client disconnected\n");
+                }
+                break;
+            }
 
-		// Echo data back to the client...
-		printf("Echoing data back to client: %s\n", buffer);
-		send(sock, buffer, len, 0);		// Echo received bytes
-		printf("Closing socket..\n");
-		close(sock);				// Close the socket
+            // Write received data to TUN interface
+            printf("Writing %d bytes to TUN\n", n);
+            write(tun_fd, buffer, n);
+        }
 
-	}		
+        // Client disconnected, close their socket
+        printf("Closing client socket..\n");
+        close(sock);
+    }
 
-	printf("Closing server listen socket..\n");
-	close (listen_sock);				// Close descriptor referencing server socket
-	
+    // Close sockets and TUN interface
+    printf("Closing server listen socket and TUN interface..\n");
+    close(listen_sock);
+    close(tun_fd);
+    
+    return 0;
 }
